333dd74022ec196cb46b5ac25a1b3daabe1e60eb
ScraftyJack <45809773+ScraftyJack@users.noreply.github.com>
2018-12-12 00:46:53 -0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  discord: {
+    token: 'NTIyMjYxNjAzNTk3NDE4NDk2.DvJGdQ.OxJwO30qg-tr7EP9D-9ZGnDfaDs',
+    feedChannelId: '522192312609013772',
+    statusChannelId: '522192312609013772'
+  },
+  guild: {
+    guilds: ['Valiant Valkyries'],
+    alliance: ''
+  },
+  battle: {
+    minPlayers: 1,
+    minRelevantPlayers: 1
+  },
+  kill: {
+    minFame: 0
+  }
+};
